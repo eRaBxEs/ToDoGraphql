c0857f2e965d6e58c77d41263e84f12571a0203b
eRaBxEs <ehiojee@yahoo.com>
2022-07-18 17:09:39 +0100

    Configure WebSocket with CORS

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/99designs/gqlgen v0.14.0
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/vektah/gqlparser/v2 v2.2.0
 )
--- server.go
+++ server.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 	"todo-graphql/graph"
 	"todo-graphql/graph/generated"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/websocket"
 )
 
 const defaultPort = "8080"
@@ -23,7 +28,30 @@ func main() {
 
 	//instead of ‘generated.Config{Resolvers: &graph.Resolver{}})’
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: graph.NewResolver()}))
+	srv := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: graph.NewResolver()}))
+	// Configure WebSocket with CORS
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+		KeepAlivePingInterval: 10 * time.Second,
+	})
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.MultipartForm{})
+
+	srv.SetQueryCache(lru.New(1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New(100),
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
